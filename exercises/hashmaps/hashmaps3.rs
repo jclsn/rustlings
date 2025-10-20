9972890aaf19460bcb926ca6dfe8c19905b835a0
Jan Claußen <jan.claussen10@web.de>
2025-10-20 13:56:18 +0200

    Completed hashmaps

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
@@ -25,8 +23,8 @@ struct Team {
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
-    // The name of the team is the key and its associated struct is the value.
     let mut scores: HashMap<String, Team> = HashMap::new();
+    let mut team_borrow;
 
     for r in results.lines() {
         let v: Vec<&str> = r.split(',').collect();
@@ -34,12 +32,26 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be the number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+
+        /* First borrow of scores occurs here */
+        team_borrow = scores.entry(team_1_name).or_insert(Team {
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+
+        team_borrow.goals_scored += team_1_score;
+        team_borrow.goals_conceded += team_2_score;
+
+        /* Second borrow of scores occurs here */
+        team_borrow = scores.entry(team_2_name).or_insert(Team {
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+
+        team_borrow.goals_scored += team_2_score;
+        team_borrow.goals_conceded += team_1_score;
     }
+
     scores
 }
 
